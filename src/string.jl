e96044f25b93fa6d6f744a75376da4d02e6d431b
Frank Otto <traktofon@gmail.com>
2017-03-30 15:31:29 +0800

    More convenience for FString type.

--- src/string.jl
+++ src/string.jl
@@ -1,4 +1,4 @@
-import Base: sizeof, read, write
+import Base: sizeof, show, convert, read, write
 
 
 const Fchar = Cchar
@@ -7,18 +7,21 @@ immutable FString{N}
    data :: Array{Fchar,1}
 end
 
+sizeof{N}(::Type{FString{N}}) = N
 sizeof{N}(::FString{N}) = N
 sizeof{N}(a::Array{FString{N}}) = N*length(a)
 
+show{N}(io::IO, ::Type{FString{N}}) = print(io, "FString{$N}")
+show{N}(io::IO, s::FString{N}) = begin print(io, "FString($N,"); show(io, trimstring(s)); print(io, ")") end
 
-function Base.convert{N}(::Type{FString{N}}, s::String)
+function convert{N}(::Type{FString{N}}, s::String)
    l = length(s)
    FString{N}( [ Fchar(i>l?' ':s[i]) for i=1:N ] )
 end
 
 FString(N, s::String) = convert( FString{N}, s )
 
-Base.convert{N}(::Type{String}, s::FString{N}) = String(map(Char,s.data))
+convert{N}(::Type{String}, s::FString{N}) = String(map(Char,s.data))
 
 
 function read{N}( io::IO, t::Type{FString{N}} )
