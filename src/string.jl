2713e492f59a99ad8f75909f8473730f1034bd81
Frank Otto <traktofon@gmail.com>
2017-04-03 13:35:18 +0800

    Documentation for FString.

--- src/string.jl
+++ src/string.jl
@@ -3,6 +3,13 @@ import Base: sizeof, print, show, convert, read, write
 
 const Fchar = Cchar
 
+"""
+    FString{N}
+
+Datatype for reading and writing character strings from `FortranFile`s.
+The type parameter `N` signifies the length of the string.
+This is the equivalent of the Fortran datatype `CHARACTER(len=N)`.
+"""
 immutable FString{N}
    data :: Array{Fchar,1}
 end
@@ -20,6 +27,13 @@ function convert{N}(::Type{FString{N}}, s::String)
    FString{N}( [ Fchar(i>l?' ':s[i]) for i=1:N ] )
 end
 
+"""
+    FString(N, s::String)
+
+Convert the Julia `String` `s` to an `FString{N}`.
+`s` must contain only ASCII characters.
+As in Fortran, the string will be padded with spaces or truncated in order to reach the desired length.
+"""
 FString(N, s::String) = convert( FString{N}, s )
 
 convert{N}(::Type{String}, s::FString{N}) = String(map(Char,s.data))
@@ -35,7 +49,12 @@ function write{N}( io::IO, s::FString{N} )
    write(io, s.data)
 end
 
+"""
+    trimstring(s::FString)
 
+Convert the `FString` `s` into a Julia `String`, where
+trailing spaces are removed. Use `String(s)` to keep the spaces.
+"""
 function trimstring{N}( s::FString{N} )
    l = N
    while l>0
@@ -44,3 +63,4 @@ function trimstring{N}( s::FString{N} )
    end
    String( map(Char, s.data[1:l]) )
 end
+
