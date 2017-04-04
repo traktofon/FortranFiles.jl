84858f5aec7b1082e158c29bb6cb7d2cc8ee6c23
Frank Otto <traktofon@gmail.com>
2017-04-04 10:46:41 +0800

    Move test data generation into runtests. Edit travis config.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,52 @@
 using FortranFiles
 using Base.Test
 
-include("common.jl")
+# To test long records being split into subrecords, without needing
+# test data files of several GB, define a custom RecordMarkerType with
+# a short maximum subrecord length
+const maxsubreclen = 2^15 - 9
+const RECMRK4Bwssr = FortranFiles.WithSubrecords(maxsubreclen)
+const RECMRK4Bwosr = FortranFiles.WithoutSubrecords{Int32}()
+
+# --- Definition of different types of record marker ---
+
+immutable RecordMarkerTypeTest
+   recmrktyp :: FortranFiles.RecordMarkerType
+   desc      :: String
+   tag       :: String
+   fflags    :: String
+end
+
+const recmrktyp_tests = [
+   RecordMarkerTypeTest( RECMRK4Bwssr, "4-byte with short subrecords",   "4Bshort", "-fmax-subrecord-length=$(maxsubreclen)" ),
+   RecordMarkerTypeTest( RECMRK4B,     "4-byte with default subrecords", "4Bdef",   "" ),
+   RecordMarkerTypeTest( RECMRK8B,     "8-byte without subrecords",      "8B",      "-frecord-marker=8" ),
+   RecordMarkerTypeTest( RECMRK4Bwosr, "4-byte without subrecords",      "4Bdumb",  "" )
+   ]
+
+# --- Generation of test data and Julia reading/writing code ---
+
+function gendata(tag, fflags)
+   cmd = `make -C codegen SUFFIX=$tag XFLAGS=$fflags`
+   run(cmd)
+   cmd = `codegen/gendata$(tag).x data$(tag).bin`
+   run(cmd)
+end
+
+function genalldata(tests)
+   for test in tests
+      gendata(test.tag, test.fflags)
+   end
+   return nothing
+end
+
+genalldata(recmrktyp_tests)
 
 include("codegen/jread.jl")
 include("codegen/jskip.jl")
 include("codegen/jwrite.jl")
+
+# --- Auxiliary functions ---
 
 function cmpfiles(a::String, b::String)
    cmd = `cmp $a $b`
@@ -17,6 +58,7 @@ function cmpfiles(a::String, b::String)
    end
 end
 
+# --- TESTING STARTS HERE ---
 
 @testset "Strings" begin
    jstr = "Hello World!"
