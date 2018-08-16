5ac2774570772750aca9b62840e372c79f606c6b
Frank Otto <traktofon@gmail.com>
2018-08-16 15:19:55 +0100

    Deleted v0.4.2 and aliases.
    As the corresponding release of the code was deleted.

--- versions.js
+++ versions.js
@@ -1,12 +1,8 @@
 var DOC_VERSIONS = [
-  "stable",
-  "latest",
-  "release-0.4",
   "release-0.3",
   "release-0.2",
   "release-0.1",
   "release-0.0",
-  "v0.4.2",
   "v0.4.1",
   "v0.4.0",
   "v0.3.0",
